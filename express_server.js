f7409effef8f53521414054f43473420e556ea09
erikleisinger <erikleisinger@gmail.com>
2020-09-28 18:26:45 +0000

    added new route to :shortURL

--- express_server.js
+++ express_server.js
@@ -31,6 +31,11 @@ app.get('/urls', (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = {shortURL: req.params.shortURL, longURL: ''};
+  res.render('urls_show', templateVars);
+})
+
 app.get("/about", (req, res) => {
   res.render('pages/about');
 });
