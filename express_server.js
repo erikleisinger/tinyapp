a2f1b85cd8d48602c752aeb5c962285197d0e25f
erikleisinger <erikleisinger@gmail.com>
2020-10-01 23:35:30 +0000

    added annotations to important functions in express_server.js

--- express_server.js
+++ express_server.js
@@ -7,7 +7,7 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 
-const { generateRandomString, validateUser, loginUser, urlsForUser, urlConverter, validateId } = require('./helpers');
+const { generateRandomString, validateUser, findUser, urlsForUser, urlConverter, validateId } = require('./helpers');
 const PORT = 3050; // default port 8080
 
 app.set('view engine', 'ejs');
@@ -63,12 +63,12 @@ app.get('/u/:shortURL', (req, res) => {
     error: null,
   };
 
-  
+
   // first check if the URL exists
   if (validateId(urlDatabase, req.params.shortURL) === false) {
     templateVars.error = `URL does not exist.`
     templateVars.user = users[req.session.user_id],
-    res.render('pages/index', templateVars);
+      res.render('pages/index', templateVars);
   }
 
   // if URL exists, redirect to page
@@ -81,7 +81,7 @@ app.post('/updateURL', (req, res) => {
     user: users[req.session.user_id],
     error: null,
   };
- 
+
   let shortURL = Object.keys(req.body)[0];
 
   // convert the link to http:// url format
@@ -93,51 +93,63 @@ app.post('/updateURL', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+
+// Create new URL
 app.post("/urls", (req, res) => {
   const templateVars = {
     urls: null,
     user: users[req.session.user_id],
     error: null,
   };
-  let randomId = generateRandomString()
+  //random string for the URL id in urlDatabase
+  let randomId = generateRandomString();
 
   // url is passed through a function which ensures it begins 'http://', to ensure no redirect problems
   let convertedUrl = urlConverter(req.body.longURL);
+
+  //new entry in urlDatabase
   urlDatabase[randomId] = { longUrl: convertedUrl, userId: req.session.user_id };
 
-  templateVars.urls = urlsForUser(urlDatabase, req.session.user_id);
 
+  templateVars.urls = urlsForUser(urlDatabase, req.session.user_id);
   res.render(`urls_index`, templateVars);
 });
 
+// delete URL
 app.post('/urls/:id/delete', (req, res) => {
   const templateVars = {
     user: users[req.session.user_id],
     error: null,
   };
-  // first check if the id exists
 
-
+  // first check if the URL exists
   if (validateId(urlDatabase, req.params.id) === false) {
-    templateVars.error = `URL does not exist.`
-    res.status(400).send('URL not found.')
+    templateVars.error = `URL does not exist.`;
+    res.status(400).send('URL not found.');
   }
 
+
+  // if URL exists..
   let userId = req.session.user_id;
+  // check URLs associated with the client's ID
   let userURLs = urlsForUser(urlDatabase, userId);
-  if (userURLs) {
-    if (userURLs[req.params.id]) {
-      console.log(`User ${userId} deleted entry { ${req.params.id}: "${urlDatabase[req.params.id]}" }`);
-      delete urlDatabase[req.params.id];
-      console.log(`Updated urls: ${JSON.stringify(urlDatabase)}`)
-    }
-  } else {
-     res.status(400).send('Access denied.')
-  }
 
+  // check that there is an entry for the client in urlDatabase
+  if (userURLs) {
+    // check client URLs for the requested URL
+    if (userURLs[req.params.id]) {
+      // if URL is in client's URLs, it will be deleted
+      delete urlDatabase[req.params.id];
+    }
+    // status code 403 if the requested URL is not the client's
+  } else {
+    res.status(403).send('Access denied.')
+  }
   res.redirect('/urls');
 });
 
+
+// PUT - NEW USER REGISTRATION
 app.post('/register', (req, res) => {
   const templateVars = {
     user: "",
@@ -145,26 +157,34 @@ app.post('/register', (req, res) => {
     error: null,
     register: null,
   };
+
+  // email and password verified using helper function validateUser()
+  // function returns a custom error depending on the problem
   let error = validateUser(users, req.body.email, req.body.password);
   if (error) {
     templateVars.error = error;
     res.render('register', templateVars);
 
   } else {
+    // hash the password received so it can be stored securely
     let hash = bcrypt.hashSync(req.body.password, saltRounds);
+    // store new user data (including hashed password) in the users database with a unique ID
     let newId = generateRandomString();
     users[newId] = {
       id: newId,
       email: req.body.email,
       password: hash
     };
-    
+
+    //assign encrypted cookie to client
+    req.session.user_id = newId;
+    // set parameters to be given to urls_index when rendered
     templateVars.email = req.body.email;
     templateVars.urls = urlsForUser(urlDatabase, newId);
     templateVars.register = true;
-    console.log(users[newId])
-    req.session.user_id = newId;
     templateVars.user = users[req.session.user_id];
+
+
     res.render('urls_index', templateVars);
   }
 });
@@ -187,26 +207,33 @@ app.post('/login', (req, res) => {
     email: null,
     register: null,
   };
-  if (loginUser(users, req.body.email, req.body.password)) {
 
-    let userId = loginUser(users, req.body.email, req.body.password);
+  // verify the user exists with the findUser() helper function
+  if (findUser(users, req.body.email, req.body.password)) {
+
+    // assign cookie to client
+    let userId = findUser(users, req.body.email, req.body.password);
     req.session.user_id = userId;
 
+    // update parameters to be passed into urls_index upon rendering
     templateVars.user = users[req.session.user_id];
     templateVars.urls = urlsForUser(urlDatabase, userId);
+
     res.render('urls_index', templateVars);
   } else {
-    console.log(`loginUser returned false`)
+    // if the user does not exist in the users database, return to login page with relevant error
     templateVars.error = `Email or password incorrect.`;
     templateVars.email = req.body.email;
+
     res.render('login', templateVars);
   }
 
 })
 
 app.post('/logout', (req, res) => {
-
+  // remove cooke from client
   req.session = null;
+
   res.redirect('/urls');
 });
 
@@ -216,12 +243,14 @@ app.get('/urls', (req, res) => {
     user: users[req.session.user_id],
     error: null,
   };
-  console.log(users);
-  console.log(req.session.user_id)
   if (req.session.user_id) {
+    // finds the clients URLs in the database using urlsForUser function
+    // passes them into urls_index as parameters so they can be displayed
     templateVars.urls = urlsForUser(urlDatabase, req.session.user_id);
+
     res.render("urls_index", templateVars);
   } else {
+    // if the client does not have a cookie, they must login
     res.redirect('login');
   }
 
@@ -236,6 +265,7 @@ app.get('/urls/new', (req, res) => {
 
     res.render('urls_new', templateVars);
   } else {
+    // redirect to login if client has no cookie
     res.redirect('/login');
   }
 
@@ -248,22 +278,19 @@ app.get('/urls/:shortURL', (req, res) => {
     user: users[req.session.user_id],
   };
 
-// first check to see if shortURL exists at all
-
+  // first check to see if shortURL exists at all
   if (validateId(urlDatabase, req.params.shortURL) == false) {
-  
-  templateVars.error = `URL does not exist.`
-  res.render('pages/index', templateVars)
-}
-  
-  let userId = req.session.user_id;
+    templateVars.error = `URL does not exist.`
+    res.render('pages/index', templateVars)
+  }
 
+  let userId = req.session.user_id;
   // checks if client is logged in
   if (userId) {
-   
+
     // if logged in, finds the client's account URLS
     let userURLs = urlsForUser(urlDatabase, userId);
-    
+
     //displays URL if client is the owner
     if (userURLs && userURLs[req.params.shortURL]) {
       res.render('urls_show', templateVars);
@@ -273,7 +300,7 @@ app.get('/urls/:shortURL', (req, res) => {
       templateVars.error = "You must be the owner of this URL to edit it";
       templateVars.urls = userURLs;
       res.render('urls_index', templateVars);
-    } 
+    }
     //if client is not logged in, they are redirected to the home page with relevant error
   } else {
     templateVars.error = `Not logged in`;
